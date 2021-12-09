9e7de5557e0b32d9a6ed6e93b6509468c220e458
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2021-12-09 15:05:13 -0300

    Create Classes04.py

---
+++ Classes/Classes04.py
@@ -0,0 +1,21 @@
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura #altura em centímetros
+    
+    def envelhecer(self, anos):
+        self.idade += anos
+        if (self.idade < 21):
+            self.altura += 0.5
+    
+    def engordar(self, ganho):
+        self.peso += ganho
+    
+    def emagrecer(self, perda):
+        self.peso += perda
+    
+    def crescer(self, ganho):
+        self.altura += ganho
+
